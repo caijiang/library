3b6e1fc4ececf17e6e63ce02104335a473cede85
Cai Jiang <luffy.ja@gmail.com>
2019-11-09 03:55:08 +0800

    make JSONStoring serial.

--- jpa/src/main/kotlin/me/jiangcai/common/jpa/type/JSONStoring.kt
+++ jpa/src/main/kotlin/me/jiangcai/common/jpa/type/JSONStoring.kt
@@ -1,5 +1,7 @@
 package me.jiangcai.common.jpa.type
 
+import java.io.Serializable
+
 /**
  * 表示一个可以在数据表中以json描述的类型
  * 通常可以一个[Map]的方式进行使用
@@ -7,7 +9,11 @@ package me.jiangcai.common.jpa.type
  */
 class JSONStoring(
     var data: Map<*, *>
-) {
+) : Serializable {
+    companion object {
+        const val serialVersionUID = -5231698596139313211L
+    }
+
     @Suppress("UNCHECKED_CAST")
     fun <X, Y> readAs(): Map<X, Y> {
         return data as Map<X, Y>
